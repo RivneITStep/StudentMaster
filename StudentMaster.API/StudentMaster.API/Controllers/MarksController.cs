8ebf9124bb45ad3b2f8c700eb88222a0540e042f
andriy22 <istep.andriy@gmail.com>
2020-01-24 23:04:45 +0200

    student can see own marks

---
+++ StudentMaster.API/StudentMaster.API/Controllers/MarksController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentMaster.BLL.Interfaces;
+
+namespace StudentMaster.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarksController : ControllerBase
+    {
+        private readonly IMarksService _marksService;
+        public MarksController(IMarksService marksService)
+        {
+            this._marksService = marksService;
+        }
+
+
+        [HttpGet("get-marks-for-chart")]
+        [Authorize]
+        public async Task<IActionResult> getMarksForChartAsync()
+        {
+            return Ok(await this._marksService.getMarksForChartByUID(User.Identity.Name));
+        }
+    }
+}
