4b369628f40f536cfb190f8413be401c819bf543
andriy22 <istep.andriy@gmail.com>
2020-01-26 22:20:24 +0200

    added  interface xD)

---
+++ StudentMaster.API/StudentMaster.BLL/Interfaces/ITeacherService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentMaster.BLL.Interfaces
+{
+    public interface ITeacherService
+    {
+        // SOMETHINGS
+    }
+}
