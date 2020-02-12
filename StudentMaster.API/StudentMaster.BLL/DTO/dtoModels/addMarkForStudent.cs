cbdb64282b09dc42827c06f57b4224fd0e3644c3
andriy22 <istep.andriy@gmail.com>
2020-02-12 17:55:16 +0200

    added loader

---
+++ StudentMaster.API/StudentMaster.BLL/DTO/dtoModels/addMarkForStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentMaster.BLL.DTO.dtoModels
+{
+    public class addMarkForStudent
+    {
+        public string uid { get; set; }
+        public string type { get; set; }
+        public int subjectId { get; set; }
+        public int mark { get; set; }
+        public DateTime date { get; set; }
+    }
+}
