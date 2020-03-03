cb8ea72781d5b229255e6e8e7d003006426f2ef6
Andriy22 <istep.andriy@gmail.com>
2020-03-03 09:49:50 +0200

    bug fix

---
+++ StudentMaster.API/StudentMaster.API/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentMaster.API.Hubs
+{
+    public class ChatHub: Hub
+    {
+
+        // Отправка сообщений
+        public void Send(string name, string message)
+        {
+            //Clients.All.SendAsync();
+        }
+
+
+  
+    }
+}
