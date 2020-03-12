7c4c84981c7daa3f53f70a2f8fed6573b7366b7b
andriy22 <istep.andriy@gmail.com>
2020-03-12 14:36:46 +0200

    added buttons

--- StudentMaster.Client/src/app/routes/news/components/news/show-new/show-new.component.ts
+++ StudentMaster.Client/src/app/routes/news/components/news/show-new/show-new.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { NewsModel } from '@core/models/news-model';
+import { AuthenticationService, adminRole } from '@core';
 
 @Component({
   selector: 'app-show-new',
@@ -8,12 +9,19 @@ import { NewsModel } from '@core/models/news-model';
   styleUrls: ['./show-new.component.css'],
 })
 export class ShowNewComponent {
+  isAdmin = false;
   constructor(
     public dialogRef: MatDialogRef<ShowNewComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: NewsModel
-  ) {}
+    @Inject(MAT_DIALOG_DATA) public data: NewsModel,
+    private authService: AuthenticationService,
+  ) {
+      this.isAdmin = this.authService.HasRole(adminRole);
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+  deleteNew(id) {
+    console.log(id);
   }
 }
